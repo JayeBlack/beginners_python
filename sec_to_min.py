44de06d9ced9ad519adad4890aba8f47024e39d5
[jayeblack] <[jayeblack19@gmail.com]>
2023-11-20 11:11:48 -0800

    commit

---
+++ sec_to_min.py
@@ -0,0 +1,4 @@
+user = int(input("Enter number of seconds:"))
+minute = user//60
+second = user%60
+print(f"{user} seconds is {minute} minute and {second} seconds")
---
+++ user's_name_choice_times.py
@@ -0,0 +1,6 @@
+user = input("Hello, enter your name here:")
+number = int(input("Enter the number of times you wanna see your name:"))
+
+
+for i in range(number):
+    print(user)
