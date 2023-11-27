5241a17235875b152bacee1da90737b840948644
[jayeblack] <[jayeblack19@gmail.com]>
2023-11-27 10:05:36 -0800

    Added File

---
+++ examples_on_squares.py
@@ -0,0 +1,27 @@
+# This Python code generates and prints the squares of numbers from 1 to 100.
+# Additionally, it keeps track of the count of squares that are multiples of 10
+count = []
+for i in range(1, 101):
+    print(i**2)
+    if i % 10 == 0:
+        count.append(i)
+
+print(f"The number of squares in 1 to 100 that are multiples of 10 are {count}")
+
+# A program that counts how many of the squares of the numbers from 1 to 100 end in a
+# 4 and how many end in a 9.
+print("************************************************************************************************")
+print("||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||")
+print("                                                                                                ")
+print("************************************************************************************************")
+
+count1 = []
+count2 = []
+for i in range(1, 101):
+    print(i**2)
+    if i % 10 == 4:
+        count1.append(i)
+    elif i % 10 == 9:
+        count2.append(i)
+
+print(f"number of squares in ending in 4s are {count1} and that of 9s are {count2}")
