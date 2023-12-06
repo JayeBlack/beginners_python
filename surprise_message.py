3719bd17e5ea092314ac22cc7bf2badf7eadd082
[jayeblack] <[jayeblack19@gmail.com]>
2023-12-06 00:44:11 -0800

    added file

--- surprise_message.py
+++ surprise_message.py
@@ -3,12 +3,17 @@ from random import randint
 
 def guess_a_number():
     scores = 0
-    message = "Hey, you mean a lot to me"
-
+    message = "em ot tol a naem uoY"
+    try:
+        name = input("Hello there! Tell us your name:")
+    except ValueError:
+        print("Please enter your name:")    
     numbers = randint(1, 10)
     for _ in range(5):
-        user = int(input("Guess a number(1-10):"))
-
+        try:
+            user = int(input("Guess a number(1-10):"))
+        except ValueError:
+            print("Please enter a number(1-7)")
         if user == numbers:
             print("You guessed the number correctly!")
             scores += 10
@@ -20,8 +25,7 @@ def guess_a_number():
 
     if scores >= 1:
         print("Congratulations! You've unlocked a surprise message.")
-        message = message[::-1]
-        print(message)
+        print(f"{name}",message)
     else:
         print("Better luck next time.")
 
